8c6487e9fa10994c87881e4faa7119520bfc10e7
Adam Weissman <adam@comedytragedyepic.com>
2019-09-17 10:33:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,15 +3,17 @@
 
 def possibly_prime(range_test, num)
   the_booleans = range_test.select {|test_number| test_number if (num % test_number == 0)}
-  the_booleans
+  return the_booleans.size > 1 ? false : true
+end
       
-
-
 def prime(num)
   if num * -1 == num || num.class != Fixnum
     return nil
   else
     test_this = (num.to_f ** .05).floor
     range_test = (1..test_this).to_a
+    return possibly_prime(range_test, num)
+  end
+end
 
     
