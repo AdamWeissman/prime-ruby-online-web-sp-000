29da19741d62cc0b83ae1ac2fa3244a88b0e9b26
Adam Weissman <adam@comedytragedyepic.com>
2019-09-17 10:24:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,25 @@
 # Add  code here!
 
+
+def possibly_prime(range_test)
+  counter = 0
+  until counter = range_test.last
+    range_test.each do |does_this_go_into|
+      if num % does_this_go_into == 0
+        return false
+        break
+      else if false
+      break
+      elsif
+    
+      
+
+
 def prime(num)
+  if num * -1 == num || num.type == 
+    return nil
+  else
   test_this = (num.to_f ** .05).floor
   range_test = (1..test_this).to_a
-  possibly_prime
+
+    
